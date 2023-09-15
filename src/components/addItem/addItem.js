4568459c3806523f4e60e925cb28b35aa31fbb71
Raniaabdullahh <raniaabdullah6600@gmail.com>
2023-09-15 18:47:48 +0300

    done

--- src/components/addItem/addItem.js
+++ src/components/addItem/addItem.js
@@ -1,9 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class addItem extends Component {
     state = {
         product: '',
-        price: ''
+        price: '',
+        quantity: 1
     }
 
     handleChange = (e) => {
@@ -18,17 +19,43 @@ class addItem extends Component {
         this.props.add(this.state)
         this.setState({
             product: '',
-            price: ''
+            price: '',
+            quantity: 1
         })
     }
 
-    render(){
-        return(
+    increaser = () => {
+        this.setState((prevState) => ({
+            quantity: prevState.quantity + 1,
+        }));
+
+    }
+    decrease = () => {
+        if (this.state.quantity > 0) {
+            this.setState((prevState) => ({
+                quantity: prevState.quantity - 1,
+            }));
+        } else {
+            this.setState((prevState) => ({
+                quantity: 0,
+            }));
+        }
+
+    }
+
+    render() {
+        return (
             <div className="item">
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" value={this.state.product} placeholder="Enter Product" id="product" onChange={this.handleChange} required/>
-                    <input type="number" value={this.state.price} placeholder="Enter Price" id="price" onChange={this.handleChange} required/>
-                    <input type="submit" value="Add"/>
+                    <input type="text" value={this.state.product} placeholder="Enter Product" id="product" onChange={this.handleChange} required />
+                    <input type="number" value={this.state.price} placeholder="Enter Price" id="price" onChange={this.handleChange} required />
+                    {/* quantity */}
+                    <div className="quantity">
+                        <button type="button" className="qbtn" onClick={this.decrease}>-</button>
+                        <input type="number" placeholder="Enter Quantity" id="quantity" value={this.state.quantity} onChange={this.handleChange} required />
+                        <button type="button" className="qbtn" onClick={this.increaser}>+</button>
+                    </div>
+                    <input type="submit" value="Add" />
                 </form>
             </div>
         )
